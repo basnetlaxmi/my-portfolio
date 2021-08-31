5112deca6767de9fc935236c5426e336d6c242d7
basnetlaxmi <ddsanzel@gmail.com>
2021-08-31 15:55:42 +0545

    Fix Linter errors

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 > This is my second project in the Microverse in which I built my portfolio . This is my responsive desktop version. The objective was to build template from figma by using CSS Flexbox. For that I created my-portfolio repo and cloned into my local machine. For best practices I have implemented linter,Github Flow and professional documentation as much as I can. For the reference, you can click [here](https://www.figma.com/file/l7SqJ3ZfkAKih9sFxvWSR4/Microverse-Student-Project-1?node-id=48%3A27) to look at the template I chose.
 
-![screenshot](./screenshot/desktop-version.png)
+![screenshot] (./screenshot/screnshot-menu.PNG)
 
 
 
--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const hamburgerPage = document.querySelector('.hamburger-page');
 const hambClose = document.querySelector('.close-ham-img');
 const otherClose = document.querySelector('.menu-inside-hamb-page');
 
-makeMenuDisappear.addEventListener ('click', show);
-
 function show() {
-    firstMainPage.style.display = 'none';
-    hamburgerPage.style.display = 'block';
+  firstMainPage.style.display = 'none';
+  hamburgerPage.style.display = 'block';
 }
-hambClose.addEventListener('click', close);
-otherClose.addEventListener('click', close);
 
 function close() {
-    firstMainPage.style.display = 'block';
-    hamburgerPage.style.display = 'none';
+  firstMainPage.style.display = 'block';
+  hamburgerPage.style.display = 'none';
 }
+
+makeMenuDisappear.addEventListener('click', show);
+hambClose.addEventListener('click', close);
+otherClose.addEventListener('click', close);
