7f2eb8acddc29250f1d471598f276831eb18a333
basnetlaxmi <ddsanzel@gmail.com>
2021-08-31 11:43:27 +0545

    Add style to hamburger menu

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const mainMenu = document.querySelector('.menu-list');
-const closeMenu= document.querySelector('.closeMenu');
+const closeMenu= document.querySelector('.close-menu');
 const openMenu= document.querySelector('.hamburger');
-const display=document.getElementsByClassName('')
+
 
 openMenu.addEventListener('click',show);
 
 closeMenu.addEventListener('click',close);
 
 function show() {
+    closeMenu.style.display='block';
     mainMenu.style.display='flex';
     mainMenu.style.top='0';
+    document.getElementById("mail").style.display="none";
+    document.getElementById("menu-icon").style.display="none";
+    
     
 }
 
 function close() {
-    mainMenu.style.top ='-100%';
+    mainMenu.style.display='none';
+    document.getElementById("menu-icon").style.display="block";
+    closeMenu.style.display='none';
+
+
 }
