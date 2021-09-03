d2fe9076bc4afc39258e9e9f4a6323864ed37590
AmineHLub <Amino.bhl@gmail.com>
2021-09-03 23:56:17 +0100

    Data now is saved on refresh

--- js/storage.js
+++ js/storage.js
@@ -1,22 +1,16 @@
 const fullName = document.getElementById('name');
 const mail = document.getElementById('email');
-const submit = document.getElementById('sub-btn');
-
 function store() {
   const data = {
     fname: fullName.value,
     email: mail.value,
   };
-
   localStorage.setItem('formData', JSON.stringify(data));
-
-  function preFill() {
-    const dataRetrieve = JSON.parse(localStorage.getItem('formData'));
-  
-    if (dataRetrieve) {
-      fullName.value = dataRetrieve.fname;
-      mail.value = dataRetrieve.email;
-    }
-  }
-  
-  submit.addEventListener('click', store, preFill);
+}
+const dataRetrieve = JSON.parse(localStorage.getItem('formData'));
+if (dataRetrieve) {
+  fullName.value = dataRetrieve.fname;
+  mail.value = dataRetrieve.email;
+}
+// eslint-disable-next-line no-undef
+form.addEventListener('input', store);
