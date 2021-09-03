966ebcb24357086e84e53cdb2a48db9c8b67bebc
basnetlaxmi <ddsanzel@gmail.com>
2021-09-04 00:09:44 +0545

    Remove prefill function

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,7 @@
 const fullName = document.getElementById('name').value;
 const mail = document.getElementById('email').value;
+const submit = document.getElementById('sub-btn');
+
 function store() {
   const data = {
     fname: fullName,
@@ -9,14 +11,4 @@ function store() {
   localStorage.setItem('formData', JSON.stringify(data));
 }
 
-function preFill() {
-  const dataRetrieve = JSON.parse(localStorage.getItem('formData'));
 
-  if (dataRetrieve) {
-    fullName.value = dataRetrieve.fname
-  }
-}
-const submit = document.getElementById('sub-btn');
-submit.addEventListener('click', store, preFill);
-
-// console.log(dataRetrieve);
