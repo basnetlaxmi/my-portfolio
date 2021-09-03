172a9f40eba90a9772e642085073f798fca50106
basnetlaxmi <ddsanzel@gmail.com>
2021-09-03 16:58:56 +0545

    Add projects content dynamically

--- js/modal.js
+++ js/modal.js
@@ -1,68 +1,86 @@
-const projectList = {
-  firstProject: {
+const projectList = [
+  {
+    projectTitle: 'Data Dashboard Healthcare',
     popupHeading: 'Multi-Post Stories 1',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
 
   },
-  secondProject: {
+  {
+    projectTitle: 'Data Dashboard Healthcare',
     popupHeading: 'Multi-Post Stories 2',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
   },
-  thirdProject: {
+  {
+    projectTitle: 'Profesional Art Printing Data',
     popupHeading: 'Multi-Post Stories 3',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
   },
 
-  fourthProject: {
+  {
+    projectTitle: 'Profesional Art Printing Data',
     popupHeading: 'Multi-Post Stories 4',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
   },
 
-  fifthProject: {
+  {
+    projectTitle: 'Profesional Art Printing Data',
     popupHeading: 'Multi-Post Stories 5',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
   },
 
-  sixthProject: {
+  {
+    projectTitle: 'Profesional Art Printing Data',
     popupHeading: 'Multi-Post Stories 6',
     languages: ['html', 'Bootstrap', 'Ruby on Rails'],
     images: 'images/mb-modal.png',
+    projectDesc: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
     liveServerbtn: 'https://basnetlaxmi.github.io/my-portfolio/',
     sourceCode: 'https://github.com/basnetlaxmi/my-portfolio ',
   },
-};
-
-const projects = [
-  projectList.firstProject,
-  projectList.secondProject,
-  projectList.thirdProject,
-  projectList.fourthProject,
-  projectList.fifthProject,
-  projectList.sixthProject,
-];
+]
 
 // eslint-disable-next-line no-unused-vars
+
+
+for (let i = 0; i < projectList.length; i += 1) {
+  //document.querySelectorAll('.project-logo')[i].src = arr[i].logo;
+  document.querySelectorAll('.project-name')[i].innerText = projectList[i].projectTitle;
+  document.querySelectorAll('.project-desc')[i].innerText = projectList[i].projectDesc;
+  const li = document.querySelector('.lanuguages-list');
+  
+  for (let i = 0; i < projectList[i].languages.length; i += 1) {
+    const list = document.createElement('li');
+    list.textContent = projectList[i].languages[i];
+    li.appendChild(list);
+  }
+}
+
 function modal(number) {
   const modalSection = document.querySelector('#modal');
   modalSection.style.display = 'flex';
@@ -74,17 +92,17 @@ function modal(number) {
   modalList.innerHTML = '';
   const modalDesc = document.querySelector('.modal-description');
 
-  modalHeading.innerText = projects[number].popupHeading;
+  modalHeading.innerText = projectList[number].popupHeading;
 
-  for (let i = 0; i < projects[number].languages.length; i += 1) {
+  for (let i = 0; i < projectList[number].languages.length; i += 1) {
     const list = document.createElement('li');
-    list.textContent = projects[number].languages[i];
+    list.textContent = projectList[number].languages[i];
     modalList.appendChild(list);
   }
 
-  modalDesc.innerHTML = projects[number].description;
-  document.querySelectorAll('.modal-btn > a')[0].href = projects[number].liveServerbtn;
-  document.querySelectorAll('.modal-btn > a')[1].href = projects[number].sourceCode;
+  modalDesc.innerHTML = projectList[number].description;
+  document.querySelectorAll('.modal-btn > a')[0].href = projectList[number].liveServerbtn;
+  document.querySelectorAll('.modal-btn > a')[1].href = projectList[number].sourceCode;
 }
 
 // eslint-disable-next-line no-unused-vars
