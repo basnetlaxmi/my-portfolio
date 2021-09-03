44d5dad2e9386a7291df23050f011c001e7d5c3d
AmineHLub <Amino.bhl@gmail.com>
2021-09-04 00:01:33 +0100

    Add textarea to the list of objects to be saved in the browser on refresh

--- js/storage.js
+++ js/storage.js
@@ -1,9 +1,11 @@
 const fullName = document.getElementById('name');
 const mail = document.getElementById('email');
+const textArea = document.getElementById('textarea');
 function store() {
   const data = {
     fname: fullName.value,
     email: mail.value,
+    textarea: textArea.value,
   };
   localStorage.setItem('formData', JSON.stringify(data));
 }
@@ -11,6 +13,7 @@ const dataRetrieve = JSON.parse(localStorage.getItem('formData'));
 if (dataRetrieve) {
   fullName.value = dataRetrieve.fname;
   mail.value = dataRetrieve.email;
+  textArea.value = dataRetrieve.textarea;
 }
 // eslint-disable-next-line no-undef
 form.addEventListener('input', store);
