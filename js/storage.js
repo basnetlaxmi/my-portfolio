b1d43baabe30a8e8f638595db817a16dec22c39a
basnetlaxmi <ddsanzel@gmail.com>
2021-09-04 00:00:00 +0545

    Add prefill function

--- js/storage.js
+++ js/storage.js
@@ -1,23 +1,22 @@
+const fullName = document.getElementById('name').value;
+const mail = document.getElementById('email').value;
 function store() {
   const data = {
-
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
+    fname: fullName,
+    email: mail,
   };
 
   localStorage.setItem('formData', JSON.stringify(data));
 }
 
 function preFill() {
-  const dataRetrieve = JSON.parse(localStorage.getItem('formData')) ;
-  
+  const dataRetrieve = JSON.parse(localStorage.getItem('formData'));
+
   if (dataRetrieve) {
-    
+    fullName.value = dataRetrieve.fname
   }
-  }
+}
 const submit = document.getElementById('sub-btn');
 submit.addEventListener('click', store, preFill);
-
-
 
 // console.log(dataRetrieve);
