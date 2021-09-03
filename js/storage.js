cdf656eaafb8efd08ef8c592cdb55a300a7947fd
basnetlaxmi <ddsanzel@gmail.com>
2021-09-03 23:18:56 +0545

    Add prefill function in storage.js

--- js/storage.js
+++ js/storage.js
@@ -7,3 +7,17 @@ function store() {
 
   localStorage.setItem('formData', JSON.stringify(data));
 }
+
+function preFill() {
+  const dataRetrieve = JSON.parse(localStorage.getItem('formData')) ;
+  
+  if (dataRetrieve) {
+    
+  }
+  }
+const submit = document.getElementById('sub-btn');
+submit.addEventListener('click', store, preFill);
+
+
+
+// console.log(dataRetrieve);
